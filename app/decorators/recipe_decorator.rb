067a7f64e0d3cccee4eae36e82825033c4677a0d
Mario Schuettel <github@lxxxvi.ch>
2017-07-22 14:47:58 +0200

    Don't display decimals in nutritions table

--- app/decorators/recipe_decorator.rb
+++ app/decorators/recipe_decorator.rb
@@ -3,60 +3,60 @@ class RecipeDecorator < Draper::Decorator
 
   # SUMS
   def sum_kcal
-    model.sum_kcal || 0.0
+    model.sum_kcal.round || 0
   end
 
   def sum_carbs
-    model.sum_carbs || 0.0
+    model.sum_carbs.round || 0
   end
 
   def sum_carbs_sugar_part
-    model.sum_carbs_sugar_part || 0.0
+    model.sum_carbs_sugar_part.round || 0
   end
 
   def sum_protein
-    model.sum_protein || 0.0
+    model.sum_protein.round || 0
   end
 
   def sum_fat
-    model.sum_fat || 0.0
+    model.sum_fat.round || 0
   end
 
   def sum_fat_saturated
-    model.sum_fat_saturated || 0.0
+    model.sum_fat_saturated.round || 0
   end
 
   def sum_fiber
-    model.sum_fiber || 0.0
+    model.sum_fiber.round || 0
   end
 
   # SERVINGS
   def serving_kcal
-    (model.serving_kcal || 0.0).round(1)
+    model.serving_kcal.round || 0
   end
 
   def serving_carbs
-    (model.serving_carbs || 0.0).round(1)
+    model.serving_carbs.round || 0
   end
 
   def serving_carbs_sugar_part
-    (model.serving_carbs_sugar_part || 0.0).round(1)
+    model.serving_carbs_sugar_part.round || 0
   end
 
   def serving_protein
-    (model.serving_protein || 0.0).round(1)
+    model.serving_protein.round || 0
   end
 
   def serving_fat
-    (model.serving_fat || 0.0).round(1)
+    model.serving_fat.round || 0
   end
 
   def serving_fat_saturated
-    (model.serving_fat_saturated || 0.0).round(1)
+    model.serving_fat_saturated.round || 0
   end
 
   def serving_fiber
-    (model.serving_fiber || 0.0).round(1)
+    model.serving_fiber.round || 0
   end
 
   # PERCENTAGES
