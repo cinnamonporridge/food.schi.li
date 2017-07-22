f5382aa903d8a8ac547d1441602d9c83864d9ee1
Mario Schuettel <github@lxxxvi.ch>
2017-07-22 15:58:39 +0200

    SMTP Settings for Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,8 +59,18 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'food.schi.li' }
+
+  config.action_mailer.smtp_settings = {
+    address: 'email-smtp.eu-west-1.amazonaws.com',
+    port: 587,
+    user_name: Rails.application.secrets.smtp['user_name'],
+    password: Rails.application.secrets.smtp['password'],
+    authentication: :login,
+    enable_starttls_auto: true
+  }
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
