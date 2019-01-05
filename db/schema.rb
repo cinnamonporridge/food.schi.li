19d869c08fd9582631100b9776b97bb5ade96ad1
Mario <lxxxvi@users.noreply.github.com>
2019-01-05 12:29:39 +0100

    add vegan field to Recipe (#17)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_05_095642) do
+ActiveRecord::Schema.define(version: 2019_01_05_112742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2019_01_05_095642) do
     t.integer "servings", default: 1, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "vegan", default: false, null: false
   end
 
   create_table "users", id: :serial, force: :cascade do |t|
