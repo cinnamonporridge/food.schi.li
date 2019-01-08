8da316ad1f0d54dea35da921c1bd53e9a1e9b7c9
lxxxvi <lxxxvi@users.noreply.github.com>
2019-01-08 16:14:48 +0100

    Journal day subnavigation

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,19 @@
-$(document).ready(function($) {
-  $('.clickable').click(function() {
-    window.location = $(this).data("href");
-  });
+let journalDaysDropdown = function() {
+  return document.querySelector('#journal_days_dropdown');
+};
+
+$('.clickable').click(function() {
+  window.location = $(this).data("href");
 });
+
+let handleJournalDaysChange = function(event) {
+  var target = event.target;
+  var selectedIndex = target.selectedIndex;
+  var selectedOption = target.options[selectedIndex];
+
+  window.location.href = selectedOption.value;
+};
+
+if(journalDaysDropdown()) {
+  journalDaysDropdown().addEventListener('change', handleJournalDaysChange);
+}
