c6b1d52a9c9ffc3d2647317367182559f8903ecf
lxxxvi <lxxxvi@users.noreply.github.com>
2019-09-27 18:01:30 +0200

    remove defaults from nutritions

---
+++ db/migrate/20190927154609_remove_defaults_from_nutritions.rb
@@ -0,0 +1,21 @@
+class RemoveDefaultsFromNutritions < ActiveRecord::Migration[6.0]
+  def up
+    change_column :nutritions, :kcal, :integer, null: false, default: nil
+    change_column :nutritions, :carbs, :decimal, precision: 10, scale: 3, null: false, default: nil
+    change_column :nutritions, :carbs_sugar_part, :decimal, precision: 10, scale: 3, null: false, default: nil
+    change_column :nutritions, :protein,:decimal, precision: 10, scale: 3, null: false, default: nil
+    change_column :nutritions, :fat, :decimal, precision: 10, scale: 3, null: false, default: nil
+    change_column :nutritions, :fat_saturated, :decimal, precision: 10, scale: 3, null: false, default: nil
+    change_column :nutritions, :fiber, :decimal, precision: 10, scale: 3, null: false, default: nil
+  end
+
+  def down
+    change_column :nutritions, :kcal, :integer, default: 0, null: false
+    change_column :nutritions, :carbs, :decimal, precision: 10, scale: 3, default: "0.0", null: false
+    change_column :nutritions, :carbs_sugar_part, :decimal, precision: 10, scale: 3, default: "0.0", null: false
+    change_column :nutritions, :protein, :decimal, precision: 10, scale: 3, default: "0.0", null: false
+    change_column :nutritions, :fat, :decimal, precision: 10, scale: 3, default: "0.0", null: false
+    change_column :nutritions, :fat_saturated, :decimal, precision: 10, scale: 3, default: "0.0", null: false
+    change_column :nutritions, :fiber, :decimal, precision: 10, scale: 3, default: "0.0", null: false
+  end
+end
