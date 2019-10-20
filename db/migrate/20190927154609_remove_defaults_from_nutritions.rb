9416f7a2794577c23df3d94e8218ce007a281d30
lxxxvi <lxxxvi@users.noreply.github.com>
2019-10-20 15:28:33 +0200

    change migrations to use bulk-option

--- db/migrate/20190927154609_remove_defaults_from_nutritions.rb
+++ db/migrate/20190927154609_remove_defaults_from_nutritions.rb
@@ -1,23 +1,25 @@
-# rubocop:disable Rails/BulkChangeTable
 class RemoveDefaultsFromNutritions < ActiveRecord::Migration[6.0]
   def up
-    change_column :nutritions, :kcal, :integer, null: false, default: nil
-    change_column :nutritions, :carbs, :decimal, precision: 10, scale: 3, null: false, default: nil
-    change_column :nutritions, :carbs_sugar_part, :decimal, precision: 10, scale: 3, null: false, default: nil
-    change_column :nutritions, :protein, :decimal, precision: 10, scale: 3, null: false, default: nil
-    change_column :nutritions, :fat, :decimal, precision: 10, scale: 3, null: false, default: nil
-    change_column :nutritions, :fat_saturated, :decimal, precision: 10, scale: 3, null: false, default: nil
-    change_column :nutritions, :fiber, :decimal, precision: 10, scale: 3, null: false, default: nil
+    change_table :nutritions, bulk: true do |t|
+      t.change :kcal, :integer, null: false, default: nil
+      t.change :carbs, :decimal, precision: 10, scale: 3, null: false, default: nil
+      t.change :carbs_sugar_part, :decimal, precision: 10, scale: 3, null: false, default: nil
+      t.change :protein, :decimal, precision: 10, scale: 3, null: false, default: nil
+      t.change :fat, :decimal, precision: 10, scale: 3, null: false, default: nil
+      t.change :fat_saturated, :decimal, precision: 10, scale: 3, null: false, default: nil
+      t.change :fiber, :decimal, precision: 10, scale: 3, null: false, default: nil
+    end
   end
 
   def down
-    change_column :nutritions, :kcal, :integer, default: 0, null: false
-    change_column :nutritions, :carbs, :decimal, precision: 10, scale: 3, default: '0.0', null: false
-    change_column :nutritions, :carbs_sugar_part, :decimal, precision: 10, scale: 3, default: '0.0', null: false
-    change_column :nutritions, :protein, :decimal, precision: 10, scale: 3, default: '0.0', null: false
-    change_column :nutritions, :fat, :decimal, precision: 10, scale: 3, default: '0.0', null: false
-    change_column :nutritions, :fat_saturated, :decimal, precision: 10, scale: 3, default: '0.0', null: false
-    change_column :nutritions, :fiber, :decimal, precision: 10, scale: 3, default: '0.0', null: false
+    change_table :nutritions, bulk: true do |t|
+      t.change :kcal, :integer, default: 0, null: false
+      t.change :carbs, :decimal, precision: 10, scale: 3, default: '0.0', null: false
+      t.change :carbs_sugar_part, :decimal, precision: 10, scale: 3, default: '0.0', null: false
+      t.change :protein, :decimal, precision: 10, scale: 3, default: '0.0', null: false
+      t.change :fat, :decimal, precision: 10, scale: 3, default: '0.0', null: false
+      t.change :fat_saturated, :decimal, precision: 10, scale: 3, default: '0.0', null: false
+      t.change :fiber, :decimal, precision: 10, scale: 3, default: '0.0', null: false
+    end
   end
 end
-# rubocop:enable Rails/BulkChangeTable
