bedbf6938aaa90f5ef2f98b4ab924e770f30770b
Mario <lxxxvi@users.noreply.github.com>
2019-10-20 15:52:27 +0200

    travis config (#38)
    * travis config
    
    * debugging travis
    
    * change migrations to use bulk-option
    
    * fix .travis.yml

--- db/migrate/20170212173224_add_reset_password_challenge_columns_to_user.rb
+++ db/migrate/20170212173224_add_reset_password_challenge_columns_to_user.rb
@@ -1,8 +1,8 @@
-# rubocop:disable Rails/BulkChangeTable
 class AddResetPasswordChallengeColumnsToUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :reset_password_link_sent_at, :datetime, null: true
-    add_column :users, :reset_password_challenge, :string, null: true
+    change_table :users, bulk: true do |t|
+      t.column :reset_password_link_sent_at, :datetime, null: true
+      t.column :reset_password_challenge, :string, null: true
+    end
   end
 end
-# rubocop:enable Rails/BulkChangeTable
--- db/migrate/20170213195807_add_magic_link_columns_to_user.rb
+++ db/migrate/20170213195807_add_magic_link_columns_to_user.rb
@@ -1,8 +1,8 @@
-# rubocop:disable Rails/BulkChangeTable
 class AddMagicLinkColumnsToUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :magic_link_sent_at, :datetime, null: true
-    add_column :users, :magic_link_challenge, :string, null: true
+    change_table :users, bulk: true do |t|
+      t.column :magic_link_sent_at, :datetime, null: true
+      t.column :magic_link_challenge, :string, null: true
+    end
   end
 end
-# rubocop:enable Rails/BulkChangeTable
