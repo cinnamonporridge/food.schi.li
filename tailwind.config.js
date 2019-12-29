54ae8406fe12902b1a2a8e90622c2043f2700021
lxxxvi <lxxxvi@users.noreply.github.com>
2019-12-29 15:05:17 +0100

    journal day nutritions

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
         '1/2': '4px'
       },
       fontSize: {
-        '2xs': '0.65rem'
+        '2xs': '0.65rem',
+        '3xs': '0.50rem',
+        '4xs': '0.30rem'
       }
     }
   },
