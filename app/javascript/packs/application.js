988ff47988cb8b6c25fd7c4ec21c7dacbe5d037c
lxxxvi <lxxxvi@users.noreply.github.com>
2020-06-26 10:02:32 +0200

    move js code from ./app/assets to ./app/javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,42 @@
 // const imagePath = (name) => images(name, true)
 
 import '../src/application.css'
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  // journal days dropdown
+  let journalDaysDropdown = document.querySelector('#journal_days_dropdown');
+
+  let handleJournalDaysChange = function(event) {
+    var target = event.target;
+    var selectedIndex = target.selectedIndex;
+    var selectedOption = target.options[selectedIndex];
+
+    window.location.href = selectedOption.value;
+  };
+
+  if(journalDaysDropdown !== null) {
+    journalDaysDropdown.addEventListener('change', handleJournalDaysChange);
+  }
+
+  // .clickable
+  let registerClickableEvent = function(element) {
+    element.addEventListener('click', function() {
+      window.location = element.dataset.href;
+    });
+  }
+
+  Array.from(document.querySelectorAll('.clickable')).forEach(registerClickableEvent);
+
+
+  // .js-toggle-actions
+  let registerToggleActionsEvent = function(element) {
+    element.addEventListener('click', function() {
+      var clickedElement = element.parentNode;
+
+      var actionsElement = clickedElement.querySelector('.actions');
+      actionsElement.classList.toggle('hidden');
+    });
+  }
+
+  Array.from(document.querySelectorAll('.js-toggle-actions')).forEach(registerToggleActionsEvent);
+});
