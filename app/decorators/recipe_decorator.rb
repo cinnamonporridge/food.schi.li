413302b17ef9fa8b5229bca009f87b1e68245e9e
lxxxvi <lxxxvi@users.noreply.github.com>
2020-10-03 17:00:43 +0200

    remove sum_* methods from Recipe

--- app/decorators/recipe_decorator.rb
+++ app/decorators/recipe_decorator.rb
@@ -5,61 +5,7 @@ class RecipeDecorator < Draper::Decorator
     "#{model.name} (#{model.servings} servings)"
   end
 
-  # SUMS
-  def sum_kcal
-    model.sum_kcal.round || 0
-  end
-
-  def sum_carbs
-    model.sum_carbs.round || 0
-  end
-
-  def sum_carbs_sugar_part
-    model.sum_carbs_sugar_part.round || 0
-  end
-
-  def sum_protein
-    model.sum_protein.round || 0
-  end
-
-  def sum_fat
-    model.sum_fat.round || 0
-  end
-
-  def sum_fat_saturated
-    model.sum_fat_saturated.round || 0
-  end
-
-  def sum_fiber
-    model.sum_fiber.round || 0
-  end
-
-  # SERVINGS
-  def serving_kcal
-    model.serving_kcal.round || 0
-  end
-
-  def serving_carbs
-    model.serving_carbs.round || 0
-  end
-
-  def serving_carbs_sugar_part
-    model.serving_carbs_sugar_part.round || 0
-  end
-
-  def serving_protein
-    model.serving_protein.round || 0
-  end
-
-  def serving_fat
-    model.serving_fat.round || 0
-  end
-
-  def serving_fat_saturated
-    model.serving_fat_saturated.round || 0
-  end
-
-  def serving_fiber
-    model.serving_fiber.round || 0
+  def serving(nutrition_fact)
+    (model[nutrition_fact] / model.servings).round || 0
   end
 end
