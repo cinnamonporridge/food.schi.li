6ce9af180689156f28f7c8a209b5618c790848ab
mario <github@lxxxvi.ch>
2022-01-11 12:00:39 +0100

    fix bug where recipe nutritions table was shown even if there were no ingredients

--- test/system/recipe_test.rb
+++ test/system/recipe_test.rb
@@ -42,4 +42,25 @@ class RecipeTest < ApplicationSystemTestCase
     assert_selector 'ul#ingredients_list', text: 'Whole Grain', count: 0
     assert_selector '.nutritions-table', text: "Per serving\n89\n"
   end
+
+  test 'deletes last ingredient from a recipe' do
+    recipe = recipes(:apple_pie)
+    ingredients(:milk_in_apple_pie).destroy! # we only want one recipe
+    assert_equal 1, recipe.ingredients.count
+
+    using_browser do
+      sign_in_user(users(:daisy))
+      navigate_to 'Recipes'
+
+      click_on 'Apple Pie'
+      assert_selector '.nutritions-table'
+
+      within find('ul#ingredients-list li', text: 'Apple') do
+        click_on "Apple"
+        click_on "Remove ingredient"
+      end
+
+      assert_text 'No ingredients = no nutritions.'
+    end
+  end
 end
