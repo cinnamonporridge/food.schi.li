5f0fedd3013372a10e40e53583fcc724381811da
mario <github@lxxxvi.ch>
2022-01-14 16:09:46 +0100

    add tests for foods to harden user scoping

--- test/controllers/foods_controller_test.rb
+++ test/controllers/foods_controller_test.rb
@@ -5,6 +5,79 @@ class FoodsControllerTest < ActionDispatch::IntegrationTest
     login_user :daisy
   end
 
+  # index
+  test 'get index shows' do
+    get foods_path
+    assert_response :success
+  end
+
+  # show
+  test 'get show of own food' do
+    get food_path(foods(:apple))
+    assert_response :success
+  end
+
+  test 'cannot get show other users food' do
+    assert_raises ActiveRecord::RecordNotFound do
+      get food_path(foods(:apricot))
+    end
+  end
+
+  # new
+  test 'get new of own food' do
+    get new_food_path
+    assert_response :success
+  end
+
+  # create
+  test 'post create creates food for own' do
+    assert_difference -> { users(:daisy).foods.count }, +1 do
+      post foods_path, params: {
+        food: {
+          name: 'Pineapple',
+          vegan: '1',
+          kcal: '74',
+          carbs: '19.58',
+          carbs_sugar_part: '14.35',
+          protein: '0.85',
+          fat: '0.19',
+          fat_saturated: '0.014',
+          fiber: '2.2'
+        }
+      }
+    end
+    follow_redirect!
+    assert_response :success
+
+    food = users(:daisy).foods.last
+    assert_equal 'Pineapple', food.name
+  end
+
+  # edit
+  test 'get edit of own food' do
+    get edit_food_path(foods(:apple))
+    assert_response :success
+  end
+
+  test 'cannot get edit of other users food' do
+    assert_raises ActiveRecord::RecordNotFound do
+      get edit_food_path(foods(:apricot))
+    end
+  end
+
+  # destroy
+  test 'delete destroy of own food' do
+    assert_difference -> { users(:daisy).foods.count }, -1 do
+      delete food_path(foods(:sugar))
+    end
+  end
+
+  test 'cannot delete destroy of other users food' do
+    assert_raises ActiveRecord::RecordNotFound do
+      delete food_path(foods(:apricot))
+    end
+  end
+
   test 'declare a food to be vegan' do
     food = foods(:apple)
 
