6885c56a3aa379c283dcd9106e657217bb205da3
mario <github@lxxxvi.ch>
2022-01-25 16:51:40 +0100

    refactor update_nutritino_facts_on_journal_days.sql to use UPDATE

--- lib/sql/update_nutrition_facts_on_journal_days.sql
+++ lib/sql/update_nutrition_facts_on_journal_days.sql
@@ -1,85 +1,39 @@
-WITH default_amount AS (
-  SELECT 100::FLOAT AS VALUE
-)
-
-, with_journal_day_meal_nutrition_facts AS (
-  SELECT jd.id                                       AS journal_day_id
-       , (mi.amount / da.value) * f.kcal             AS journal_day_meal_kcal
-       , (mi.amount / da.value) * f.carbs            AS journal_day_meal_carbs
-       , (mi.amount / da.value) * f.carbs_sugar_part AS journal_day_meal_carbs_sugar_part
-       , (mi.amount / da.value) * f.protein          AS journal_day_meal_protein
-       , (mi.amount / da.value) * f.fat              AS journal_day_meal_fat
-       , (mi.amount / da.value) * f.fat_saturated    AS journal_day_meal_fat_saturated
-       , (mi.amount / da.value) * f.fiber            AS journal_day_meal_fiber
+WITH with_journal_day_meal_nutrition_facts AS (
+  SELECT jd.id                                               AS journal_day_id
+       , (mi.amount / d.default_amount) * f.kcal             AS kcal
+       , (mi.amount / d.default_amount) * f.carbs            AS carbs
+       , (mi.amount / d.default_amount) * f.carbs_sugar_part AS carbs_sugar_part
+       , (mi.amount / d.default_amount) * f.protein          AS protein
+       , (mi.amount / d.default_amount) * f.fat              AS fat
+       , (mi.amount / d.default_amount) * f.fat_saturated    AS fat_saturated
+       , (mi.amount / d.default_amount) * f.fiber            AS fiber
     FROM journal_days jd
-   CROSS JOIN default_amount da
-   LEFT OUTER JOIN meals m                  ON m.journal_day_id = jd.id
-   LEFT OUTER JOIN meal_ingredients mi      ON mi.meal_id = m.id
-   LEFT OUTER JOIN portions p               ON p.id = mi.portion_id
-   LEFT OUTER JOIN foods f                  ON f.id = p.food_id
+   CROSS JOIN (SELECT 100 AS default_amount) AS d
+    LEFT OUTER JOIN meals m                  ON m.journal_day_id = jd.id
+    LEFT OUTER JOIN meal_ingredients mi      ON mi.meal_id = m.id
+    LEFT OUTER JOIN portions p               ON p.id = mi.portion_id
+    LEFT OUTER JOIN foods f                  ON f.id = p.food_id
 )
-
 , with_summed_nutrition_facts AS (
    SELECT journal_day_id
-        , sum(journal_day_meal_kcal)             AS journal_day_kcal_sum
-        , sum(journal_day_meal_carbs)            AS journal_day_carbs_sum
-        , sum(journal_day_meal_carbs_sugar_part) AS journal_day_carbs_sugar_part_sum
-        , sum(journal_day_meal_protein)          AS journal_day_protein_sum
-        , sum(journal_day_meal_fat)              AS journal_day_fat_sum
-        , sum(journal_day_meal_fat_saturated)    AS journal_day_fat_saturated_sum
-        , sum(journal_day_meal_fiber)            AS journal_day_fiber_sum
+        , COALESCE(ROUND(sum(kcal)            , 0), 0) AS kcal
+        , COALESCE(ROUND(sum(carbs)           , 3), 0) AS carbs
+        , COALESCE(ROUND(sum(carbs_sugar_part), 3), 0) AS carbs_sugar_part
+        , COALESCE(ROUND(sum(protein)         , 3), 0) AS protein
+        , COALESCE(ROUND(sum(fat)             , 3), 0) AS fat
+        , COALESCE(ROUND(sum(fat_saturated)   , 3), 0) AS fat_saturated
+        , COALESCE(ROUND(sum(fiber)           , 3), 0) AS fiber
      FROM with_journal_day_meal_nutrition_facts
     GROUP BY journal_day_id
 )
-
-, with_rounded_target_nutrution_facts AS (
-   SELECT journal_day_id
-        , ROUND(journal_day_kcal_sum::NUMERIC, 0)             AS journal_day_target_kcal
-        , ROUND(journal_day_carbs_sum::NUMERIC, 3)            AS journal_day_target_carbs
-        , ROUND(journal_day_carbs_sugar_part_sum::NUMERIC, 3) AS journal_day_target_carbs_sugar_part
-        , ROUND(journal_day_protein_sum::NUMERIC, 3)          AS journal_day_target_protein
-        , ROUND(journal_day_fat_sum::NUMERIC, 3)              AS journal_day_target_fat
-        , ROUND(journal_day_fat_saturated_sum::NUMERIC, 3)    AS journal_day_target_fat_saturated
-        , ROUND(journal_day_fiber_sum::NUMERIC, 3)            AS journal_day_target_fiber
-    FROM with_summed_nutrition_facts
-)
-
-INSERT INTO journal_days (
-    id
-  , user_id
-  , date
-  , kcal
-  , carbs
-  , carbs_sugar_part
-  , protein
-  , fat
-  , fat_saturated
-  , fiber
-  , created_at
-  , updated_at
-)
-
-SELECT jd.id
-     , jd.user_id
-     , jd.date
-     , COALESCE(tnf.journal_day_target_kcal, 0)             AS journal_day_target_kcal
-     , COALESCE(tnf.journal_day_target_carbs, 0)            AS journal_day_target_carbs
-     , COALESCE(tnf.journal_day_target_carbs_sugar_part, 0) AS journal_day_target_carbs_sugar_part
-     , COALESCE(tnf.journal_day_target_protein, 0)          AS journal_day_target_protein
-     , COALESCE(tnf.journal_day_target_fat, 0)              AS journal_day_target_fat
-     , COALESCE(tnf.journal_day_target_fat_saturated, 0)    AS journal_day_target_fat_saturated
-     , COALESCE(tnf.journal_day_target_fiber, 0)            AS journal_day_target_fiber
-     , jd.created_at
-     , NOW() AS updated_at
-  FROM with_rounded_target_nutrution_facts tnf
- INNER JOIN journal_days jd ON jd.id = tnf.journal_day_id
-    ON CONFLICT (id)
-    DO UPDATE SET kcal = EXCLUDED.kcal
-                , carbs = EXCLUDED.carbs
-                , carbs_sugar_part = EXCLUDED.carbs_sugar_part
-                , protein = EXCLUDED.protein
-                , fat = EXCLUDED.fat
-                , fat_saturated = EXCLUDED.fat_saturated
-                , fiber = EXCLUDED.fiber
-                , updated_at = EXCLUDED.updated_at
+UPDATE journal_days jd
+   SET kcal             = target.kcal
+     , carbs            = target.carbs
+     , carbs_sugar_part = target.carbs_sugar_part
+     , protein          = target.protein
+     , fat              = target.fat
+     , fat_saturated    = target.fat_saturated
+     , fiber            = target.fiber
+  FROM with_summed_nutrition_facts target
+ WHERE jd.id = target.journal_day_id
 ;
