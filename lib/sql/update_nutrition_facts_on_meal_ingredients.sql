d6fac36a3d4d7c420ea35027964257c44b0c78da
mario <github@lxxxvi.ch>
2022-01-25 17:50:59 +0100

    refactor update_nutrition_facts_on_meal_ingredients.sql to use UPDATE

--- lib/sql/update_nutrition_facts_on_meal_ingredients.sql
+++ lib/sql/update_nutrition_facts_on_meal_ingredients.sql
@@ -1,82 +1,24 @@
-WITH default_amount AS (
-  SELECT 100::FLOAT AS value
-),
-
-with_target_nutrition_facts AS (
-  SELECT mi.id                                       AS meal_ingredient_id
-       , mi.meal_id                                  AS meal_id
-       , mi.portion_id                               AS meal_portion_id
-       , mi.amount                                   AS meal_amount
-       , mi.measure                                  AS meal_measure
-       , (mi.amount / da.value) * f.kcal             AS meal_target_kcal
-       , (mi.amount / da.value) * f.carbs            AS meal_target_carbs
-       , (mi.amount / da.value) * f.carbs_sugar_part AS meal_target_carbs_sugar_part
-       , (mi.amount / da.value) * f.protein          AS meal_target_protein
-       , (mi.amount / da.value) * f.fat              AS meal_target_fat
-       , (mi.amount / da.value) * f.fat_saturated    AS meal_target_fat_saturated
-       , (mi.amount / da.value) * f.fiber            AS meal_target_fiber
-       , mi.created_at                               AS meal_created_at
-    FROM meal_ingredients mi
-   CROSS JOIN default_amount da
-   INNER JOIN portions p ON p.id = mi.portion_id
-   INNER JOIN foods f    ON f.id = p.food_id
- ),
-
- with_rounded_nutrition_facts AS (
-   SELECT meal_ingredient_id
-        , meal_id
-        , meal_portion_id
-        , meal_amount
-        , meal_measure
-        , ROUND(meal_target_kcal::NUMERIC, 0)             AS meal_target_kcal
-        , ROUND(meal_target_carbs::NUMERIC, 3)            AS meal_target_carbs
-        , ROUND(meal_target_carbs_sugar_part::NUMERIC, 3) AS meal_target_carbs_sugar_part
-        , ROUND(meal_target_protein::NUMERIC, 3)          AS meal_target_protein
-        , ROUND(meal_target_fat::NUMERIC, 3)              AS meal_target_fat
-        , ROUND(meal_target_fat_saturated::NUMERIC, 3)    AS meal_target_fat_saturated
-        , ROUND(meal_target_fiber::NUMERIC, 3)            AS meal_target_fiber
-        , meal_created_at
-     FROM with_target_nutrition_facts
- )
-
- INSERT INTO meal_ingredients (
-     id
-   , meal_id
-   , portion_id
-   , amount
-   , measure
-   , kcal
-   , carbs
-   , carbs_sugar_part
-   , protein
-   , fat
-   , fat_saturated
-   , fiber
-   , created_at
-   , updated_at
- )
- SELECT meal_ingredient_id
-      , meal_id
-      , meal_portion_id
-      , meal_amount
-      , meal_measure
-      , meal_target_kcal
-      , meal_target_carbs
-      , meal_target_carbs_sugar_part
-      , meal_target_protein
-      , meal_target_fat
-      , meal_target_fat_saturated
-      , meal_target_fiber
-      , meal_created_at
-      , NOW() AS updated_at
-   FROM with_rounded_nutrition_facts
-     ON CONFLICT (id)
-     DO UPDATE SET kcal = EXCLUDED.kcal
-                 , carbs = EXCLUDED.carbs
-                 , carbs_sugar_part = EXCLUDED.carbs_sugar_part
-                 , protein = EXCLUDED.protein
-                 , fat = EXCLUDED.fat
-                 , fat_saturated = EXCLUDED.fat_saturated
-                 , fiber = EXCLUDED.fiber
-                 , updated_at = EXCLUDED.updated_at
+UPDATE meal_ingredients mi
+  SET kcal             = target.kcal
+    , carbs            = target.carbs
+    , carbs_sugar_part = target.carbs_sugar_part
+    , protein          = target.protein
+    , fat              = target.fat
+    , fat_saturated    = target.fat_saturated
+    , fiber            = target.fiber
+  FROM (
+          SELECT mi.id
+               , ROUND(((mi.amount / d.default_amount) * f.kcal)             , 0) AS kcal
+               , ROUND(((mi.amount / d.default_amount) * f.carbs)            , 3) AS carbs
+               , ROUND(((mi.amount / d.default_amount) * f.carbs_sugar_part) , 3) AS carbs_sugar_part
+               , ROUND(((mi.amount / d.default_amount) * f.protein)          , 3) AS protein
+               , ROUND(((mi.amount / d.default_amount) * f.fat)              , 3) AS fat
+               , ROUND(((mi.amount / d.default_amount) * f.fat_saturated)    , 3) AS fat_saturated
+               , ROUND(((mi.amount / d.default_amount) * f.fiber)            , 3) AS fiber
+            FROM meal_ingredients mi
+           CROSS JOIN (SELECT 100 AS default_amount) AS d
+           INNER JOIN portions p ON p.id = mi.portion_id
+           INNER JOIN foods f    ON f.id = p.food_id
+        ) target
+WHERE mi.id = target.id
 ;
--- lib/sql/update_nutrition_facts_on_recipes.sql
+++ lib/sql/update_nutrition_facts_on_recipes.sql
@@ -1,86 +1,38 @@
-WITH default_amount AS (
-  SELECT 100::FLOAT AS value
-),
-
-with_nutrition_facts AS (
-  SELECT r.id                                       AS recipe_id
-       , (i.amount / da.value) * f.kcal             AS recipe_target_kcal
-       , (i.amount / da.value) * f.carbs            AS recipe_target_carbs
-       , (i.amount / da.value) * f.carbs_sugar_part AS recipe_target_carbs_sugar_part
-       , (i.amount / da.value) * f.protein          AS recipe_target_protein
-       , (i.amount / da.value) * f.fat              AS recipe_target_fat
-       , (i.amount / da.value) * f.fat_saturated    AS recipe_target_fat_saturated
-       , (i.amount / da.value) * f.fiber            AS recipe_target_fiber
+WITH with_nutrition_facts AS (
+  SELECT r.id                                               AS recipe_id
+       , (i.amount / d.default_amount) * f.kcal             AS kcal
+       , (i.amount / d.default_amount) * f.carbs            AS carbs
+       , (i.amount / d.default_amount) * f.carbs_sugar_part AS carbs_sugar_part
+       , (i.amount / d.default_amount) * f.protein          AS protein
+       , (i.amount / d.default_amount) * f.fat              AS fat
+       , (i.amount / d.default_amount) * f.fat_saturated    AS fat_saturated
+       , (i.amount / d.default_amount) * f.fiber            AS fiber
     FROM recipes r
-   CROSS JOIN default_amount da
+   CROSS JOIN (SELECT 100 AS default_amount) AS d
    INNER JOIN ingredients i ON i.recipe_id = r.id
    INNER JOIN portions p    ON p.id = i.portion_id
    INNER JOIN foods f       ON f.id = p.food_id
-),
-
-with_summed_nutrition_facts AS (
-  SELECT recipe_id                           AS recipe_id
-       , sum(recipe_target_kcal)             AS recipe_target_kcal_sum
-       , sum(recipe_target_carbs)            AS recipe_target_carbs_sum
-       , sum(recipe_target_carbs_sugar_part) AS recipe_target_carbs_sugar_part_sum
-       , sum(recipe_target_protein)          AS recipe_target_protein_sum
-       , sum(recipe_target_fat)              AS recipe_target_fat_sum
-       , sum(recipe_target_fat_saturated)    AS recipe_target_fat_saturated_sum
-       , sum(recipe_target_fiber)            AS recipe_target_fiber_sum
+)
+, with_summed_nutrition_facts AS (
+  SELECT recipe_id                         AS recipe_id
+       , ROUND(sum(kcal)              , 0) AS kcal
+       , ROUND(sum(carbs)             , 3) AS carbs
+       , ROUND(sum(carbs_sugar_part)  , 3) AS carbs_sugar_part
+       , ROUND(sum(protein)           , 3) AS protein
+       , ROUND(sum(fat)               , 3) AS fat
+       , ROUND(sum(fat_saturated)     , 3) AS fat_saturated
+       , ROUND(sum(fiber)             , 3) AS fiber
     FROM with_nutrition_facts
    GROUP BY recipe_id
-),
-
-with_rounded_target_nutrution_facts AS (
-SELECT recipe_id
-     , ROUND(recipe_target_kcal_sum::NUMERIC, 0)             AS recipe_target_kcal
-     , ROUND(recipe_target_carbs_sum::NUMERIC, 3)            AS recipe_target_carbs
-     , ROUND(recipe_target_carbs_sugar_part_sum::NUMERIC, 3) AS recipe_target_carbs_sugar_part
-     , ROUND(recipe_target_protein_sum::NUMERIC, 3)          AS recipe_target_protein
-     , ROUND(recipe_target_fat_sum::NUMERIC, 3)              AS recipe_target_fat
-     , ROUND(recipe_target_fat_saturated_sum::NUMERIC, 3)    AS recipe_target_fat_saturated
-     , ROUND(recipe_target_fiber_sum::NUMERIC, 3)            AS recipe_target_fiber
-  FROM with_summed_nutrition_facts
 )
-INSERT INTO recipes (
-    id
-  , name
-  , servings
-  , vegan
-  , kcal
-  , carbs
-  , carbs_sugar_part
-  , protein
-  , fat
-  , fat_saturated
-  , fiber
-  , created_at
-  , updated_at
-  , user_id
-)
-SELECT r.id
-     , r.name
-     , r.servings
-     , r.vegan
-     , tnf.recipe_target_kcal
-     , tnf.recipe_target_carbs
-     , tnf.recipe_target_carbs_sugar_part
-     , tnf.recipe_target_protein
-     , tnf.recipe_target_fat
-     , tnf.recipe_target_fat_saturated
-     , tnf.recipe_target_fiber
-     , r.created_at
-     , NOW() AS updated_at
-     , r.user_id
-  FROM with_rounded_target_nutrution_facts tnf
- INNER JOIN recipes r ON r.id = tnf.recipe_id
-    ON CONFLICT (id)
-    DO UPDATE SET kcal = EXCLUDED.kcal
-                , carbs = EXCLUDED.carbs
-                , carbs_sugar_part = EXCLUDED.carbs_sugar_part
-                , protein = EXCLUDED.protein
-                , fat = EXCLUDED.fat
-                , fat_saturated = EXCLUDED.fat_saturated
-                , fiber = EXCLUDED.fiber
-                , updated_at = EXCLUDED.updated_at
+UPDATE recipes r
+   SET kcal             = target.kcal
+     , carbs            = target.carbs
+     , carbs_sugar_part = target.carbs_sugar_part
+     , protein          = target.protein
+     , fat              = target.fat
+     , fat_saturated    = target.fat_saturated
+     , fiber            = target.fiber
+  FROM with_summed_nutrition_facts target
+ WHERE r.id = target.recipe_id
 ;
