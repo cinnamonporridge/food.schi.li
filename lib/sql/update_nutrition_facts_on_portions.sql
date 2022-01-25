056b436807ae6bfc6357f7c3ea37daefa7c73a07
mario <github@lxxxvi.ch>
2022-01-25 16:25:38 +0100

    refactor update_nutrition_facts_on_portions to use UPDATE

--- lib/sql/update_nutrition_facts_on_portions.sql
+++ lib/sql/update_nutrition_facts_on_portions.sql
@@ -1,94 +1,21 @@
-WITH default_amount AS (
-  SELECT 100::FLOAT AS value
-),
-
-portions_with_nutrition_facts AS (
-  SELECT p.id                 AS portion_id
-       , da.value             AS default_amount
-       , f.id                 AS food_id
-       , f.name               AS food_name
-       , f.kcal               AS food_kcal
-       , f.carbs              AS food_carbs
-       , f.carbs_sugar_part   AS food_carbs_sugar_part
-       , f.protein            AS food_protein
-       , f.fat                AS food_fat
-       , f.fat_saturated      AS food_fat_saturated
-       , f.fiber              AS food_fiber
-    FROM portions p
-   CROSS JOIN default_amount da
-   INNER JOIN foods f ON f.id = p.food_id
-                     AND p.amount = da.value
-    /* {{PORTION_FILTER}} */
-),
-
-with_target_nutrition_facts AS (
-  SELECT p.id                                                             AS portion_id
-       , p.amount                                                         AS portion_amount
-       , p.name                                                           AS portion_name
-       , p.food_id                                                        AS food_id
-       , (pwf.food_kcal / pwf.default_amount) * p.amount                  AS target_portion_kcal
-       , (pwf.food_carbs / pwf.default_amount) * p.amount                 AS target_portion_carbs
-       , (pwf.food_carbs_sugar_part / pwf.default_amount) * p.amount      AS target_portion_carbs_sugar_part
-       , (pwf.food_protein / pwf.default_amount) * p.amount               AS target_portion_protein
-       , (pwf.food_fat / pwf.default_amount) * p.amount                   AS target_portion_fat
-       , (pwf.food_fat_saturated / pwf.default_amount) * p.amount         AS target_portion_fat_saturated
-       , (pwf.food_fiber / pwf.default_amount) * p.amount                 AS target_portion_fiber
-       , p.created_at                                                     AS portion_created_at
-    FROM portions p
-   INNER JOIN portions_with_nutrition_facts pwf ON p.food_id = pwf.food_id
- ),
-
- with_rounded_target_nutrution_facts AS (
-   SELECT portion_id                                         AS portion_id
-        , portion_amount                                     AS portion_amount
-        , portion_name                                       AS portion_name
-        , food_id                                            AS food_id
-        , ROUND(target_portion_kcal::NUMERIC, 0)             AS target_portion_kcal
-        , ROUND(target_portion_carbs::NUMERIC, 3)            AS target_portion_carbs
-        , ROUND(target_portion_carbs_sugar_part::NUMERIC, 3) AS target_portion_carbs_sugar_part
-        , ROUND(target_portion_protein::NUMERIC, 3)          AS target_portion_protein
-        , ROUND(target_portion_fat::NUMERIC, 3)              AS target_portion_fat
-        , ROUND(target_portion_fat_saturated::NUMERIC, 3)    AS target_portion_fat_saturated
-        , ROUND(target_portion_fiber::NUMERIC, 3)            AS target_portion_fiber
-        , portion_created_at                                 AS portion_created_at
-     FROM with_target_nutrition_facts
-)
-INSERT INTO portions (
-    id
-  , name
-  , food_id
-  , amount
-  , kcal
-  , carbs
-  , carbs_sugar_part
-  , protein
-  , fat
-  , fat_saturated
-  , fiber
-  , created_at
-  , updated_at
-)
-SELECT portion_id
-     , portion_name
-     , food_id
-     , portion_amount
-     , target_portion_kcal
-     , target_portion_carbs
-     , target_portion_carbs_sugar_part
-     , target_portion_protein
-     , target_portion_fat
-     , target_portion_fat_saturated
-     , target_portion_fiber
-     , portion_created_at
-     , NOW() AS updated_at
-  FROM with_rounded_target_nutrution_facts
-    ON CONFLICT (id)
-    DO UPDATE SET kcal = EXCLUDED.kcal
-                , carbs = EXCLUDED.carbs
-                , carbs_sugar_part = EXCLUDED.carbs_sugar_part
-                , protein = EXCLUDED.protein
-                , fat = EXCLUDED.fat
-                , fat_saturated = EXCLUDED.fat_saturated
-                , fiber = EXCLUDED.fiber
-                , updated_at = EXCLUDED.updated_at
+UPDATE portions p
+   SET kcal = t.kcal
+     , carbs = t.carbs
+     , carbs_sugar_part = t.carbs_sugar_part
+     , protein = t.protein
+     , fat = t.fat
+     , fat_saturated = t.fat_saturated
+     , fiber = t.fiber
+  FROM (  SELECT p.id
+               , ROUND(((f.kcal::NUMERIC             / d.default_amount) * p.amount), 0) AS kcal
+               , ROUND(((f.carbs::NUMERIC            / d.default_amount) * p.amount), 3) AS carbs
+               , ROUND(((f.carbs_sugar_part::NUMERIC / d.default_amount) * p.amount), 3) AS carbs_sugar_part
+               , ROUND(((f.protein::NUMERIC          / d.default_amount) * p.amount), 3) AS protein
+               , ROUND(((f.fat::NUMERIC              / d.default_amount) * p.amount), 3) AS fat
+               , ROUND(((f.fat_saturated::NUMERIC    / d.default_amount) * p.amount), 3) AS fat_saturated
+               , ROUND(((f.fiber::NUMERIC            / d.default_amount) * p.amount), 3) AS fiber
+            FROM portions p
+           CROSS JOIN (SELECT 100 AS default_amount) AS d
+           INNER JOIN foods f ON f.id = p.food_id ) t
+ WHERE p.id = t.id
 ;
