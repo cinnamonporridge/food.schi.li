34d9d506c5f88b6eab9360831b83795d54868dcf
mario <github@lxxxvi.ch>
2022-02-06 17:48:03 +0100

    add Recipe::IngredientsComponentTest

--- test/components/recipe/ingredients_component_test.rb
+++ test/components/recipe/ingredients_component_test.rb
@@ -2,6 +2,9 @@ require 'test_helper'
 
 class Recipe::IngredientsComponentTest < ViewComponent::TestCase
   test '#render' do
-    assert false, 'todo'
+    render_inline new_component(recipe: recipes(:apple_pie))
+    assert_selector 'h2', text: 'Ingredients'
+    assert_text '6 servings'
+    assert_selector 'ul.recipe--ingredients'
   end
 end
