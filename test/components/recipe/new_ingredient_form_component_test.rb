720fbe58912176ab463372160a8a7763450d52b7
mario <github@lxxxvi.ch>
2022-02-06 17:31:06 +0100

    add Recipe::NewIngredientFormComponentTest

--- test/components/recipe/new_ingredient_form_component_test.rb
+++ test/components/recipe/new_ingredient_form_component_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class Recipe::NewIngredientFormComponentTest < ViewComponent::TestCase
+  test '#render, only search field' do
+    user = users(:daisy)
+    recipe_ingredient = RecipeIngredient.new(recipe: recipes(:apple_pie))
+    params = ActionController::Parameters.new(action_url: '/foo')
+    render_inline new_component(user:, recipe_ingredient:, params:)
+
+    assert_field 'Search food'
+    assert_no_button 'Add ingredient'
+  end
+
   test '#render' do
-    assert false, 'todo'
+    user = users(:daisy)
+    recipe_ingredient = RecipeIngredient.new(recipe: recipes(:apple_pie))
+    params = ActionController::Parameters.new(action_url: '/foo', food_name: 'Banana')
+    render_inline new_component(user:, recipe_ingredient:, params:)
+
+    assert_field 'Search food'
+    assert_button 'Add ingredient'
   end
 end
