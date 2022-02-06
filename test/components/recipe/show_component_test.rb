09e9c5ad0e3f3a2b85160e0bd79877f32fe2f744
mario <github@lxxxvi.ch>
2022-02-06 17:23:47 +0100

    add Recipe::ShowComponentTest

--- test/components/recipe/show_component_test.rb
+++ test/components/recipe/show_component_test.rb
@@ -2,6 +2,9 @@ require 'test_helper'
 
 class Recipe::ShowComponentTest < ViewComponent::TestCase
   test '#render' do
-    assert false, 'todo'
+    render_inline new_component(recipe: recipes(:apple_pie))
+    assert_selector 'h1', text: 'Apple Pie'
+    assert_selector 'h2', text: 'Nutritions'
+    assert_selector 'h2', text: 'Ingredients'
   end
 end
