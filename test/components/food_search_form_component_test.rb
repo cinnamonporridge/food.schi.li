2f7f3fe765bb0bac9b7406a0f3c9ff665329dc76
mario <github@lxxxvi.ch>
2022-02-06 11:02:33 +0100

    implement FoodSearchFormComponentTest

--- test/components/food_search_form_component_test.rb
+++ test/components/food_search_form_component_test.rb
@@ -1,11 +1,26 @@
 require 'test_helper'
 
-class Forms::FoodSearchComponentTest < ViewComponent::TestCase
-  test '#render' do
-    assert false, 'todo'
+class FoodSearchFormComponentTest < ViewComponent::TestCase
+  test '#render, empty :food_name' do
+    render_inline new_component(form: FoodSearchForm.new(Food.new, { action_url: 'foo' }))
+    assert_not page.find_field('Search food').value.present?
+  end
+
+  test '#render, with :food_name' do
+    render_inline new_component(form: FoodSearchForm.new(Food.new, { action_url: 'foo', food_name: 'Mango' }))
+    assert_field 'Search food', with: 'Mango'
+  end
+
+  test '#render, with foods name' do
+    render_inline new_component(form: FoodSearchForm.new(foods(:apple), { action_url: 'foo' }))
+    assert_field 'Search food', with: 'Apple'
   end
 
   test '#food_datalist_options' do
-    assert false, 'todo'
+    component = new_component(form: FoodSearchForm.new(foods(:apple), { action_url: 'foo' }))
+    assert_includes component.food_datalist_options, 'Apple'
+    assert_not_includes component.food_datalist_options,
+                        'Apricot',
+                        'Apricot belongs to John, but only Daisys foods should be listed'
   end
 end
