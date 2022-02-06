875aeb43818a08e43492a85796121e2a46480cc7
mario <github@lxxxvi.ch>
2022-02-06 17:36:37 +0100

    add Recipe::HeaderComponentTest

--- test/components/recipe/header_component_test.rb
+++ test/components/recipe/header_component_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class Recipe::HeaderComponentTest < ViewComponent::TestCase
-  test '#render' do
-    assert false, 'todo'
+  test '#render, no vegan' do
+    render_inline new_component(recipe: recipes(:apple_pie))
+    assert_selector 'h1', text: 'Apple Pie'
+    assert_no_text 'Vegan'
+    assert_link 'Edit recipe'
+    assert_link 'Copy recipe'
+    assert_button 'Archive recipe'
+  end
+
+  test '#render, vegan' do
+    render_inline new_component(recipe: recipes(:vegan_peanut_butter_banana))
+    assert_selector 'h1', text: 'Vegan Peanut Butter Banana'
+    assert_selector '.vegan-badge'
+    assert_text 'Vegan'
+    assert_link 'Edit recipe'
+    assert_link 'Copy recipe'
+    assert_button 'Archive recipe'
   end
 end
