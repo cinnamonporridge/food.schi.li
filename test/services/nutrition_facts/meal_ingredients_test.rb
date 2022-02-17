7246eeea254b526eae0e59fe1464473ecf42baf1
mario <github@lxxxvi.ch>
2022-02-17 10:52:16 +0100

    wip, add NutritionFacs::MealIngredientsTest

--- test/services/nutrition_facts/meal_ingredients_test.rb
+++ test/services/nutrition_facts/meal_ingredients_test.rb
@@ -1,19 +1,57 @@
 require 'test_helper'
 
 class NutritionFacts::MealIngredientsTest < ActiveSupport::TestCase
-  test '.update_track! record is a User' do
-    assert false, 'TODO'
+  include NutritionFactsTestHelper
+
+  test '.call! record is a Food' do
+    with_snapshots(
+      milk_on_february_first: meal_ingredients(:daisys_glass_of_milk_meal_ingredient_on_february_first),
+      milk_on_february_fifth: meal_ingredients(:daisys_milk_from_apple_pie_meal_ingredient_on_february_fifth)
+    ) do |targets|
+      falsify_all_nutrition_facts!
+      new_object(record: foods(:milk)).call!
+
+      assert_original_equals_snapshot targets[:milk_on_february_first]
+      assert_original_equals_snapshot targets[:milk_on_february_fifth]
+    end
   end
 
-  test '.update_track! record is a Food' do
-    assert false, 'TODO'
+  test '.call! record is a Portion' do
+    with_snapshots(
+      milk_on_february_first: meal_ingredients(:daisys_glass_of_milk_meal_ingredient_on_february_first),
+      milk_on_february_fifth: meal_ingredients(:daisys_milk_from_apple_pie_meal_ingredient_on_february_fifth)
+    ) do |targets|
+      falsify_all_nutrition_facts!
+      new_object(record: portions(:milk_default_portion)).call!
+
+      assert_original_equals_snapshot targets[:milk_on_february_first]
+      assert_original_equals_snapshot targets[:milk_on_february_fifth]
+    end
   end
 
-  test '.update_track! record is a Portion' do
-    assert false, 'TODO'
+  test '.call! record is a MealIngredient' do
+    with_snapshots(
+      milk_on_february_first: meal_ingredients(:daisys_glass_of_milk_meal_ingredient_on_february_first),
+      milk_on_february_fifth: meal_ingredients(:daisys_milk_from_apple_pie_meal_ingredient_on_february_fifth)
+    ) do |targets|
+      falsify_all_nutrition_facts!
+      new_object(record: meal_ingredients(:daisys_glass_of_milk_meal_ingredient_on_february_first)).call!
+
+      assert_original_equals_snapshot targets[:milk_on_february_first]
+      assert_original_equals_false targets[:milk_on_february_fifth]
+    end
   end
 
-  test '.update_track! record is a MealIngredient' do
-    assert false, 'TODO'
+  test '.call! record is a User' do
+    with_snapshots(
+      daisys_apples_on_february_first: meal_ingredients(:daisys_big_apple_meal_ingredient_on_february_first),
+      johns_apple_on_january_first: meal_ingredients(:johns_apple_on_january_first)
+    ) do |targets|
+      falsify_all_nutrition_facts!
+      new_object(record: users(:daisy)).call!
+
+      assert_original_equals_snapshot targets[:daisys_apples_on_february_first]
+      assert_original_equals_false targets[:johns_apple_on_january_first]
+    end
   end
 end
