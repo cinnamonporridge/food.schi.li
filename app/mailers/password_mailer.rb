77b3db6861a78c05c25ae4e544a62d1db59b7c82
mario <github@lxxxvi.ch>
2022-03-02 14:15:59 +0100

    wip, forms DONE

--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -1,6 +1,6 @@
 class PasswordMailer < ApplicationMailer
   def reset_link_mail(user)
     @user = user
-    mail(to: @user.email, subject: 'Your password reset link')
+    mail(to: @user.email, subject: t('.your_password_reset_link'))
   end
 end
