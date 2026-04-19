18b52faf2ac8fcb6b89aa306936b4e5726906f95
mario <github@lxxxvi.ch>
2026-04-19 10:56:07 +0200

    add host option to pg_dump

--- test/services/backup_database_test.rb
+++ test/services/backup_database_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class BackupDatabaseTest < ActiveSupport::TestCase
-  test "#call without username" do
+  test "#call without username, password and host" do
     service = BackupDatabase.new
 
     # override private #execute method to prevent system call
@@ -11,7 +11,7 @@ class BackupDatabaseTest < ActiveSupport::TestCase
 
     # override private #database_configuration method to simulate username being present
     def service.database_configuration
-      ApplicationRecord.connection_db_config.configuration_hash.dup.without(:username)
+      ApplicationRecord.connection_db_config.configuration_hash.dup.without(:username, :password, :host)
     end
 
     expected_env_variables = {}
@@ -34,11 +34,13 @@ class BackupDatabaseTest < ActiveSupport::TestCase
     # override private #database_configuration method to simulate username being present
     def service.database_configuration
       ApplicationRecord.connection_db_config.configuration_hash.dup.merge(username: "fake_user",
-                                                                          password: "fake_password")
+                                                                          password: "fake_password",
+                                                                          host: "fake-host")
     end
 
     expected_env_variables = { "PGPASSWORD" => "fake_password" }
-    expected_command = %r{pg_dump --username fake_user --dbname [^ ]+ --file /tmp/food/test\.latest\.dump}
+    expected_command =
+      %r{pg_dump --username fake_user --dbname [^ ]+ --host fake-host --file /tmp/food/test\.latest\.dump}
 
     service.call.tap do |env_variables, command|
       assert_equal expected_env_variables, env_variables
