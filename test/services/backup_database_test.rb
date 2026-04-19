9ffbea8ea7e69121d714ddd87cdf99910e7a0d42
mario <github@lxxxvi.ch>
2026-04-19 14:32:45 +0200

    raise if system call fails

--- test/services/backup_database_test.rb
+++ test/services/backup_database_test.rb
@@ -4,8 +4,8 @@ class BackupDatabaseTest < ActiveSupport::TestCase
   test "#call without username, password and host" do
     service = BackupDatabase.new
 
-    # override private #execute method to prevent system call
-    def service.execute(env_variables, command)
+    # override private #execute! method to prevent system call
+    def service.execute!(env_variables, command)
       [env_variables, command]
     end
 
@@ -15,7 +15,7 @@ class BackupDatabaseTest < ActiveSupport::TestCase
     end
 
     expected_env_variables = {}
-    expected_command = %r{pg_dump --dbname [^ ]+ --file /tmp/food/test\.latest\.dump}
+    expected_command = %r{pg_dump --no-password --dbname [^ ]+ --file /tmp/food/test\.latest\.dump}
 
     service.call.tap do |env_variables, command|
       assert_equal expected_env_variables, env_variables
@@ -26,8 +26,8 @@ class BackupDatabaseTest < ActiveSupport::TestCase
   test "#call with username" do
     service = BackupDatabase.new
 
-    # override private #execute method to prevent system call
-    def service.execute(env_variables, command)
+    # override private #execute! method to prevent system call
+    def service.execute!(env_variables, command)
       [env_variables, command]
     end
 
@@ -40,7 +40,7 @@ class BackupDatabaseTest < ActiveSupport::TestCase
 
     expected_env_variables = { "PGPASSWORD" => "fake_password" }
     expected_command =
-      %r{pg_dump --username fake_user --dbname [^ ]+ --host fake-host --file /tmp/food/test\.latest\.dump}
+      %r{pg_dump --no-password --username fake_user --dbname [^ ]+ --host fake-host --file /tmp/food/test\.latest\.dump}
 
     service.call.tap do |env_variables, command|
       assert_equal expected_env_variables, env_variables
